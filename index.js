86198838ca36df6ad9ba2b763a08c21bacb371a8
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-17 04:15:24 +0600

    created first api

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', (req, res) => res.sendFile(__dirname + '/users.json'))
+
 app.listen(port, (err, res) => {
     if (err) {
         console.log(err)
