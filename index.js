ff3e7e79e2f55dc3352c905ed3eaad8b63c47c86
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-17 23:09:24 +0600

    added search query

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/fruits', (req, res) => res.send(['apple', 'banana', 'orange']))
+app.get('/fruits/mango/fazli', (req, res) => res.send('Hello Fazli!'))
+
 // Load the api files
-app.get('/users', (req, res) => res.sendFile(__dirname + '/users.json'))
+app.get('/users', (req, res) => {
+    const search = req.query.search;
+    search ? res.send(user.filter(user => user.name.toLowerCase().includes(search))) : res.sendFile(__dirname + '/users.json');
+    // res.sendFile(__dirname + '/users.json')
+})
 
 // dynamic api
 app.get('/users/:id', (req, res) => res.send(user[req.params.id]))
